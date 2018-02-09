fade04e93e396459fefe07eb82a57f2c4e7f7959
henryledinhduy <henryledinhduy@gmail.com>
2018-02-09 18:54:58 +0200

    Redefine Spaces Indentation

--- init.el
+++ init.el
@@ -3,6 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+
+ ;; Indentation Setup for Bash Shell. 
+ '(sh-basic-offset 2)
+ '(sh-indentation 2)
+ '(smie-indent-basic 2)
+ ;; end
  '(company-quickhelp-color-background "#4F4F4F")
  '(company-quickhelp-color-foreground "#DCDCCC")
  '(custom-enabled-themes (quote (wombat)))
@@ -47,7 +53,7 @@
  ;; If there is more than one, they won't work right.
  )
 
-;; The above are automatic lines, don't edit them. 
+;; The above are automatic lines, don't mess with them if you don't know how 
 
 (setq inhibit-startup-message t)
 
@@ -118,5 +124,4 @@
 (use-package yasnippet
   :ensure t
   :init (yas-global-mode 1))
-
 
